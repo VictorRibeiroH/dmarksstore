99e68ddab27c055324f649d071e05f6a173a7b8c
Victor Ribeiro <contato.victoribeiro@gmail.com>
2023-07-20 20:51:03 -0300

    hero settings page and finish footer/nav stls

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,18 @@
+import Image from 'next/image'
+
 import React from 'react'
 import styles from './footer.module.css'
+import Link from 'next/link'
+
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>©2023 dMarks. All rights reserved.</div>
-      <div></div>
+      <div className={styles.social}>
+           <Image src="/1.png" width={15} height={15} className={styles.icon} alt="dMarks facebook"/>
+           <Image src="/2.png" width={15} height={15} className={styles.icon} alt="dMarks instagram"/>
+      </div>
     </div>
   )
 }
