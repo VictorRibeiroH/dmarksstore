99c92a82575b29b6f6a93c3e591e56b6fc9586cc
Victor Ribeiro <contato.victoribeiro@gmail.com>
2023-07-20 20:19:19 -0300

    style global module css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,9 @@
+"use client";
+
 import Link from 'next/link'
 import React from 'react'
+
+import styles from './navbar.module.css'
 
 const links = [
   {
@@ -36,12 +40,22 @@ const links = [
 
 const Navbar = () => {
   return (
-    <div>
-      <Link href="/">dMarks</Link>
-      <div>
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>dMarks</Link>
+      <div className={styles.links}>
         {links.map(link=>(
-          <Link key={link.id} href={link.url}>{link.title}</Link>
+          <Link key={link.id} href={link.url} className={styles.link}>
+            {link.title}
+          </Link>
         ))}
+        <button 
+          className={styles.logout}
+          onClick={() => {
+            console.log("logged out")
+          }}
+        >
+          Logout
+        </button>
       </div>
     </div>
   )
